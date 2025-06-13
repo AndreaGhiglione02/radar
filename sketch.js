2750f64b35416d1b4b28e9be089535a6e4ab4d16
AndreaGhiglione02 <91989265+AndreaGhiglione02@users.noreply.github.com>
2025-06-13 23:00:32 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -38,7 +38,14 @@ if (!!window.EventSource) {
   }, false);
 }
 
+
+
+
 //-----------------------------------------------
+
+function fireFunction() {
+    document.getElementById('fireAudio').play();
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -187,3 +194,36 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+/*
+
+var contextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
+var dance = document.getElementById("fireButton");
+if (contextClass) {
+  var context = new contextClass();
+} else {
+  onError;
+}
+var request = new XMLHttpRequest();
+request.open('GET', "https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/gonna-make-you-sweat.mp3", true);
+request.responseType = 'arraybuffer';
+request.onload = function() {
+ context.decodeAudioData(request.response, function(theBuffer) {
+  buffer = theBuffer;
+  }, onError);
+}
+request.send();
+
+function onError() { console.log("Bad browser! No Web Audio API for you"); }
+
+function unpress() { dance.classList.remove("pressed"); }
+
+function playSound() {
+ dance.classList.add("pressed");
+  var source = context.createBufferSource();
+  source.buffer = buffer;
+ source.connect(context.destination);
+  source.start(0);
+  var delay = 2000;
+  setTimeout(unpress,delay);
+}
+dance.addEventListener('click', function(event) { playSound(); }); */
